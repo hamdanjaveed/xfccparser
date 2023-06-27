d6ce2a7bb097e56d5600ffcd6de3261dd0d6abad
Hamdan <hamdan@squareup.com>
2023-06-26 22:19:57 -0400

    chore: upgrade to participle/v2

--- lexer.go
+++ lexer.go
@@ -1,13 +1,13 @@
 package xfccparser
 
 import (
-	"bufio"
-	"bytes"
-	"fmt"
-	"io"
-	"strings"
+  "bufio"
+  "bytes"
+  "fmt"
+  "io"
+  "strings"
 
-	"github.com/alecthomas/participle/lexer"
+  "github.com/alecthomas/participle/v2/lexer"
 )
 
 type xfccDefinition struct {
@@ -15,116 +15,116 @@ type xfccDefinition struct {
 
 var _ lexer.Definition = &xfccDefinition{}
 
-func (x *xfccDefinition) Lex(reader io.Reader) (lexer.Lexer, error) {
-	return &xfccLexer{
-		reader:     bufio.NewReader(reader),
-		separators: "=;,",
-	}, nil
+func (x *xfccDefinition) Lex(filename string, reader io.Reader) (lexer.Lexer, error) {
+  return &xfccLexer{
+    reader:     bufio.NewReader(reader),
+    separators: "=;,",
+  }, nil
 }
 
-func (x *xfccDefinition) Symbols() map[string]rune {
-	return map[string]rune{
-		"EOF":    tokenEOF,
-		"String": tokString,
-		"Char":   tokChar,
-	}
+func (x *xfccDefinition) Symbols() map[string]lexer.TokenType {
+  return map[string]lexer.TokenType{
+    "EOF":    lexer.TokenType(tokenEOF),
+    "String": lexer.TokenType(tokString),
+    "Char":   lexer.TokenType(tokChar),
+  }
 }
 
 var (
-	tokenEOF  = rune(0)
-	tokString = rune(1)
-	tokChar   = rune(2)
+  tokenEOF  = rune(0)
+  tokString = rune(1)
+  tokChar   = rune(2)
 )
 
 type xfccLexer struct {
-	reader     *bufio.Reader
-	separators string
+  reader     *bufio.Reader
+  separators string
 
-	buf   bytes.Buffer
-	start int
-	pos   int
+  buf   bytes.Buffer
+  start int
+  pos   int
 }
 
 var _ lexer.Lexer = &xfccLexer{}
 
 func (x *xfccLexer) Next() (lexer.Token, error) {
-	quoting := false
-	escaping := false
+  quoting := false
+  escaping := false
 
-	token := func(tokenType rune) lexer.Token {
-		tok := lexer.Token{
-			Type:  tokenType,
-			Value: x.buf.String(),
-			Pos:   lexer.Position{Column: x.start},
-		}
+  token := func(tokenType lexer.TokenType) lexer.Token {
+    tok := lexer.Token{
+      Type:  tokenType,
+      Value: x.buf.String(),
+      Pos:   lexer.Position{Column: x.start},
+    }
 
-		x.buf.Reset()
-		x.start = x.pos
+    x.buf.Reset()
+    x.start = x.pos
 
-		return tok
-	}
+    return tok
+  }
 
-	for {
-		char, _, err := x.reader.ReadRune()
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			return lexer.Token{}, err
-		}
-		x.pos++
+  for {
+    char, _, err := x.reader.ReadRune()
+    if err == io.EOF {
+      break
+    } else if err != nil {
+      return lexer.Token{}, err
+    }
+    x.pos++
 
-		if escaping {
-			if char == '\\' {
-				if quoting {
-					x.buf.WriteRune(char)
-					x.buf.WriteRune(char)
-				} else {
-					x.buf.WriteRune(char)
-				}
-			} else if quoting && char == '"' {
-				x.buf.WriteRune(char)
-			} else if quoting && char == ',' {
-				x.buf.WriteRune('\\')
-				x.buf.WriteRune(char)
-			} else if !quoting && strings.ContainsRune(x.separators, char) {
-				x.buf.WriteRune(char)
-			} else {
-				return lexer.Token{}, fmt.Errorf("invalid escape character `%v` (pos %d)", char, x.pos)
-			}
-			escaping = false
-		} else if char == '\\' {
-			escaping = true
-		} else if char == '"' {
-			if quoting {
-				quoting = false
-				return token(tokString), nil
-			} else {
-				quoting = true
-			}
-		} else if !quoting && strings.ContainsRune(x.separators, char) {
-			if x.buf.Len() > 0 {
-				if err := x.reader.UnreadRune(); err != nil {
-					return lexer.Token{}, err
-				}
-				return token(tokString), nil
-			}
-			x.buf.WriteRune(char)
-			return token(tokChar), nil
-		} else {
-			x.buf.WriteRune(char)
-		}
-	}
+    if escaping {
+      if char == '\\' {
+        if quoting {
+          x.buf.WriteRune(char)
+          x.buf.WriteRune(char)
+        } else {
+          x.buf.WriteRune(char)
+        }
+      } else if quoting && char == '"' {
+        x.buf.WriteRune(char)
+      } else if quoting && char == ',' {
+        x.buf.WriteRune('\\')
+        x.buf.WriteRune(char)
+      } else if !quoting && strings.ContainsRune(x.separators, char) {
+        x.buf.WriteRune(char)
+      } else {
+        return lexer.Token{}, fmt.Errorf("invalid escape character `%v` (pos %d)", char, x.pos)
+      }
+      escaping = false
+    } else if char == '\\' {
+      escaping = true
+    } else if char == '"' {
+      if quoting {
+        quoting = false
+        return token(lexer.TokenType(tokString)), nil
+      } else {
+        quoting = true
+      }
+    } else if !quoting && strings.ContainsRune(x.separators, char) {
+      if x.buf.Len() > 0 {
+        if err := x.reader.UnreadRune(); err != nil {
+          return lexer.Token{}, err
+        }
+        return token(lexer.TokenType(tokString)), nil
+      }
+      x.buf.WriteRune(char)
+      return token(lexer.TokenType(tokChar)), nil
+    } else {
+      x.buf.WriteRune(char)
+    }
+  }
 
-	if quoting {
-		return lexer.Token{}, fmt.Errorf("string missing end quote (pos %d)", x.pos)
-	}
-	if escaping {
-		return lexer.Token{}, fmt.Errorf("string missing escape (pos %d)", x.pos)
-	}
+  if quoting {
+    return lexer.Token{}, fmt.Errorf("string missing end quote (pos %d)", x.pos)
+  }
+  if escaping {
+    return lexer.Token{}, fmt.Errorf("string missing escape (pos %d)", x.pos)
+  }
 
-	if x.buf.Len() > 0 {
-		return token(tokString), nil
-	}
+  if x.buf.Len() > 0 {
+    return token(lexer.TokenType(tokString)), nil
+  }
 
-	return token(lexer.EOF), nil
+  return token(lexer.EOF), nil
 }
--- parser.go
+++ parser.go
@@ -1,145 +1,144 @@
 package xfccparser
 
 import (
-	"crypto/x509/pkix"
-	"fmt"
-
-	"github.com/alecthomas/participle"
+  "crypto/x509/pkix"
+  "fmt"
+  "github.com/alecthomas/participle/v2"
 )
 
 const (
-	// ForwardedClientCertHeader is the name of the HTTP header Envoy uses to pass metadata about certificates presented by a client
-	ForwardedClientCertHeader = "x-forwarded-client-cert"
+  // ForwardedClientCertHeader is the name of the HTTP header Envoy uses to pass metadata about certificates presented by a client
+  ForwardedClientCertHeader = "x-forwarded-client-cert"
 )
 
 // ClientCert is a client certificate passed to Envoy
 type ClientCert struct {
-	By      string
-	Hash    string
-	Cert    string
-	Chain   string
-	Subject *pkix.Name
-	URI     string
-	DNS     []string
+  By      string
+  Hash    string
+  Cert    string
+  Chain   string
+  Subject *pkix.Name
+  URI     string
+  DNS     []string
 }
 
 // ParseXFCCHeader parses an x-forwarded-client-cert header and returns the list of certificates present.
 // The format of the header is documented here: https://www.envoyproxy.io/docs/envoy/latest/configuration/http/http_conn_man/headers#x-forwarded-client-cert
 func ParseXFCCHeader(header string) ([]*ClientCert, error) {
-	groups, err := parseRawXFCCHeader(header)
-	if err != nil {
-		return nil, err
-	}
+  groups, err := parseRawXFCCHeader(header)
+  if err != nil {
+    return nil, err
+  }
 
-	if len(groups.FieldGroups) == 0 {
-		return nil, nil
-	}
+  if len(groups.FieldGroups) == 0 {
+    return nil, nil
+  }
 
-	certs := make([]*ClientCert, 0, len(groups.FieldGroups))
-	for i, group := range groups.FieldGroups {
-		cert := &ClientCert{}
+  certs := make([]*ClientCert, 0, len(groups.FieldGroups))
+  for i, group := range groups.FieldGroups {
+    cert := &ClientCert{}
 
-		for _, field := range group.Fields {
-			switch field.Key {
-			case "By":
-				cert.By = field.Value
-			case "Hash":
-				cert.Hash = field.Value
-			case "Cert":
-				cert.Cert = field.Value
-			case "Chain":
-				cert.Chain = field.Value
-			case "Subject":
-				subject, err := ParseSubject(field.Value)
-				if err != nil {
-					return nil, fmt.Errorf("invalid subject in client cert %d: %v", i, err)
-				}
-				cert.Subject = subject
-			case "URI":
-				cert.URI = field.Value
-			case "DNS":
-				cert.DNS = append(cert.DNS, field.Value)
-			default:
-				return nil, fmt.Errorf("unknown field %s in client cert %d", field.Key, i)
-			}
-		}
-		certs = append(certs, cert)
-	}
+    for _, field := range group.Fields {
+      switch field.Key {
+      case "By":
+        cert.By = field.Value
+      case "Hash":
+        cert.Hash = field.Value
+      case "Cert":
+        cert.Cert = field.Value
+      case "Chain":
+        cert.Chain = field.Value
+      case "Subject":
+        subject, err := ParseSubject(field.Value)
+        if err != nil {
+          return nil, fmt.Errorf("invalid subject in client cert %d: %v", i, err)
+        }
+        cert.Subject = subject
+      case "URI":
+        cert.URI = field.Value
+      case "DNS":
+        cert.DNS = append(cert.DNS, field.Value)
+      default:
+        return nil, fmt.Errorf("unknown field %s in client cert %d", field.Key, i)
+      }
+    }
+    certs = append(certs, cert)
+  }
 
-	return certs, nil
+  return certs, nil
 }
 
 // ParseSubject parses the subject string that is parse of the x-forwarded-client-cert header
 func ParseSubject(subject string) (*pkix.Name, error) {
-	if subject == "" {
-		return nil, nil
-	}
+  if subject == "" {
+    return nil, nil
+  }
 
-	group := &subjectFields{}
-	if err := subjectParser.ParseString(subject, group); err != nil {
-		return nil, fmt.Errorf("invalid subject: %v", err)
-	}
+  group, err := subjectParser.ParseString("parser", subject)
+  if err != nil {
+    return nil, fmt.Errorf("invalid subject: %v", err)
+  }
 
-	name := &pkix.Name{}
-	for _, field := range group.Fields {
-		switch field.Key {
-		case "C":
-			name.Country = append(name.Country, field.Value)
-		case "O":
-			name.Organization = append(name.Organization, field.Value)
-		case "OU":
-			name.OrganizationalUnit = append(name.OrganizationalUnit, field.Value)
-		case "CN":
-			name.CommonName = field.Value
-		case "SERIALNUMBER":
-			name.SerialNumber = field.Value
-		case "L":
-			name.Locality = append(name.Locality, field.Value)
-		case "ST":
-			name.Province = append(name.Province, field.Value)
-		case "STREET":
-			name.StreetAddress = append(name.StreetAddress, field.Value)
-		case "POSTALCODE":
-			name.PostalCode = append(name.PostalCode, field.Value)
-		default:
-			return nil, fmt.Errorf("unknown subject DN `%s`", field.Key)
-		}
-	}
+  name := &pkix.Name{}
+  for _, field := range group.Fields {
+    switch field.Key {
+    case "C":
+      name.Country = append(name.Country, field.Value)
+    case "O":
+      name.Organization = append(name.Organization, field.Value)
+    case "OU":
+      name.OrganizationalUnit = append(name.OrganizationalUnit, field.Value)
+    case "CN":
+      name.CommonName = field.Value
+    case "SERIALNUMBER":
+      name.SerialNumber = field.Value
+    case "L":
+      name.Locality = append(name.Locality, field.Value)
+    case "ST":
+      name.Province = append(name.Province, field.Value)
+    case "STREET":
+      name.StreetAddress = append(name.StreetAddress, field.Value)
+    case "POSTALCODE":
+      name.PostalCode = append(name.PostalCode, field.Value)
+    default:
+      return nil, fmt.Errorf("unknown subject DN `%s`", field.Key)
+    }
+  }
 
-	return name, nil
+  return name, nil
 }
 
 // parseRawXFCCHeader parses a x-forwarded-client-cert header header but does not marshal it into ClientCerts.
 func parseRawXFCCHeader(header string) (*certs, error) {
-	if header == "" {
-		return &certs{}, nil
-	}
-	groups := &certs{}
-	if err := parser.ParseString(header, groups); err != nil {
-		return nil, fmt.Errorf("invalid header format: %v", err)
-	}
+  if header == "" {
+    return &certs{}, nil
+  }
+  groups, err := parser.ParseString("parser", header)
+  if err != nil {
+    return nil, fmt.Errorf("invalid header format: %v", err)
+  }
 
-	return groups, nil
+  return groups, nil
 }
 
 // certs
 type certs struct {
-	FieldGroups []certFields `parser:"(@@ ( ',' @@ )* )?"`
+  FieldGroups []certFields `parser:"(@@ ( ',' @@ )* )?"`
 }
 
 type certFields struct {
-	Fields []field `parser:"(@@ ( ';' @@ )* )?"`
+  Fields []field `parser:"(@@ ( ';' @@ )* )?"`
 }
 
 type field struct {
-	Key   string `parser:"@String '='"`
-	Value string `parser:"(@String)?"`
+  Key   string `parser:"@String '='"`
+  Value string `parser:"(@String)?"`
 }
 
-var parser = participle.MustBuild(&certs{}, participle.Lexer(&xfccDefinition{}))
+var parser = participle.MustBuild[certs](participle.Lexer(&xfccDefinition{}))
 
 type subjectFields struct {
-	Fields []field `parser:"(@@ ( ',' @@ )* )?"`
+  Fields []field `parser:"(@@ ( ',' @@ )* )?"`
 }
 
-var subjectParser = participle.MustBuild(&subjectFields{}, participle.Lexer(&xfccDefinition{}))
+var subjectParser = participle.MustBuild[subjectFields](participle.Lexer(&xfccDefinition{}))
